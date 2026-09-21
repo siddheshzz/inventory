9f32673335d789ed472fd85c9caf7b6edda38de7
Siddhesh Z <siddheshzope1023@gmail.com>
2026-09-22 00:00:38 +0530

    Update DTO

--- src/main/java/com/siddhesh/inventoryManagement/domain/dtos/order/CreateOrderRequest.java
+++ src/main/java/com/siddhesh/inventoryManagement/domain/dtos/order/CreateOrderRequest.java
@@ -1,9 +1,17 @@
 package com.siddhesh.inventoryManagement.domain.dtos.order;
 
+import lombok.*;
+
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class CreateOrderRequest {
     private List<CreateOrderItemRequest> items;
     private UUID shippingAddressId;
